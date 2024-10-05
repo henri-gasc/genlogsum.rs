fd2b2a96630b50c24d443ccca2577beac1bc78ca
Henri Gasc <gasc@eurecom.fr>
2024-10-05 10:26:09 +0200

    This warrant a version 1.0.0 no ?

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,25 @@
 //!
 //! Collection of function used in the binary (`gls` crate).
 
+// // // // // // // // // // // // // // // // // // // // // // // //
+//
+// genlogsum: GENtoo LOG SUMmary, summarize log to show running emerge
+// Copyright (C) 2024 Henri GASC
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// // // // // // // // // // // // // // // // // // // // // // // //
+
 use std::{collections::HashMap, fs};
 
 pub use crate::json::read_mtimedb;
